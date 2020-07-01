c86a5da9382e3b3b3fe8f126635625e3789648fb
Luke Kozak <kozakluke@gmail.com>
2020-07-01 11:43:39 +0200

    Create vimeoVideo function

---
+++ src/vimeoVideo.ts
@@ -0,0 +1,16 @@
+import {removeLastFrom} from '@kozakl/utils/string';
+import {vimeoVideoId} from './index';
+
+export default async function vimeoVideo(url:string)
+{
+    const id = vimeoVideoId(url);
+    return await fetch(`http://vimeo.com/api/v2/video/${id}.json`)
+        .then((response)=> response.json())
+        .then((result)=> ({
+            title: result[0].title,
+            description: result[0].description,
+            thumbnail: removeLastFrom(result[0].thumbnail_large, '_'),
+            width: result[0].width,
+            height: result[0].height
+        }));
+}
